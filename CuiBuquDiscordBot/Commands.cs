1303853c510ba4dadb4fce06953c10512800f1fd
Joyce <joylss.633@gmail.com>
2019-11-25 10:54:47 +0800

    添加项目文件。

---
+++ CuiBuquDiscordBot/Commands.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace CuiBuquDiscordBot
+{
+    public class Commands : ModuleBase
+    {
+        [Command("hello")]
+        public async Task Hello()
+        {
+            await ReplyAsync("Hello, I am QuQu bot. Have you seen Feng-er?");
+        }
+
+        [Command("fx?")]
+        public async Task WhereIsFengXiao()
+        {
+            await ReplyAsync("Feng-er is being naughty spamming on Twitter at @FENGXlAO");
+        }
+
+        [Command("qqhelp")]
+        public async Task HelpMeQuQu()
+        {
+            await ReplyAsync("My Commands \n ==========================\n" +
+            " QuQu Bot can be summoned with the prefix 'ququ', 'QuQu' or simply mentioning me.\n\n" +
+            "1. hello - for a message\n" +
+            "2. fx? - to ask where Feng Xiao is\n" +
+            "3. dm <mention a user> - to make QuQu bot dm them (Warning: Do not misuse this to spam)\n" +
+            "4. join <mention a role> - to join a role\n" +
+            "5. leave <mention a role> - to leave a role\n");
+        }
+
+        [Command("dm")]
+        public async Task DM(SocketGuildUser _mention, string _msg = null)
+        {
+            if (_mention != null)
+            {
+                var channel = await _mention.GetOrCreateDMChannelAsync();
+                await channel.SendMessageAsync(_msg);
+            }
+        }
+
+        [Command("join")]
+        public async Task GiveRole(SocketRole _role)
+        {
+            var fxs = Context.Guild.Roles.FirstOrDefault(x => x.Name == "FengXiaoStan");
+            var ywss = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Yan Wushi Squad");
+            var member = Context.Guild.Roles.FirstOrDefault(x => x.Name == "member");
+            if (_role == fxs || _role == ywss || _role == member)
+            {
+                var msg = Context.Message;
+                await msg.DeleteAsync();
+                var user = Context.User;
+                var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == _role.ToString());
+                await (user as IGuildUser).AddRoleAsync(role);
+                await ReplyAsync("<@" + user.Id + "> has been given the role " + role.Mention);
+            }
+        }
+
+        [Command("leave")]
+        public async Task RemoveRole(SocketRole _role)
+        {
+            if (_role != null)
+            {
+                var msg = Context.Message;
+                await msg.DeleteAsync();
+                var user = Context.User;
+                var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == _role.ToString());
+                await (user as IGuildUser).RemoveRoleAsync(role);
+                await ReplyAsync("The role " + role.Mention + " has been removed from user <@" + user.Id + ">");
+            }
+        }
+
+        //[Command("kick")]
+        //public async Task Kick(SocketGuildUser mention, string reason = null)
+        //{
+        //    if(mention != null)
+        //    {
+        //        var channel = await mention.GetOrCreateDMChannelAsync();
+        //        System.Console.WriteLine(mention.Username);
+        //        await mention.KickAsync();
+        //        await channel.SendMessageAsync("You have been kicked from Joyce's dummy server for no reason.");
+        //    }
+        //}
+    }
+}
---
+++ CuiBuquDiscordBot/Program.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CuiBuquDiscordBot
+{
+    public class Program
+    {
+        //NTY3MTcwMjU5NjI3NDA5NDA4.XdeIDw.kCZ0t8sGM0TrRgLkGvY4l3Gum1M
+
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+        {
+            Client = new DiscordSocketClient();
+
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, "NTY3MTcwMjU5NjI3NDA5NDA4.XdeIDw.kCZ0t8sGM0TrRgLkGvY4l3Gum1M", true);
+
+            await Client.StartAsync();
+
+            await Handler.Install(Client);
+
+            Client.Ready += Client_Ready;
+            Client.UserJoined += AnnounceJoinedUser;
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Ready()
+        {
+            Console.Write("Bot is ready.");  
+            return;
+        }
+
+        public async Task AnnounceJoinedUser(SocketGuildUser user) //Welcomes the new user
+        {
+            var channel = Client.GetChannel(633166864486367233) as SocketTextChannel;
+            await channel.SendMessageAsync($"Welcome {user.Username} to {channel.Guild.Name}, I am QuQu, please check out <#633502666248421396> then introduce yourself in <#633497759286296577> to get your roles.");
+        }
+    }
+}
